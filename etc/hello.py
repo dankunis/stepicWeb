7e45fbdcc465e5abf45d8ee58a57f868f119bbb8
dankunis <32678968+dankunis@users.noreply.github.com>
2018-04-24 14:04:40 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,12 @@
-def app(environ, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return body
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web',
+    # 'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--daemon',
+        '--workers=2',
+        '--timeout=60',
+        'hello:app',
+    ),
+}
