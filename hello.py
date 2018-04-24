e375195f2e307cd2685d29f4224caaa77eced152
dankunis <32678968+dankunis@users.noreply.github.com>
2018-04-24 21:58:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = '\n'.join([i for i in environ['QUERY_STRING'].split('&')])
+    start_response(status, headers)
+    return [body.encode()]
