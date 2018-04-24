d77f533170048235c547517a3713996c37e313a4
dankunis <32678968+dankunis@users.noreply.github.com>
2018-04-24 22:00:35 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -D -c /etc/gunicorn.d/hello.py hello:app
