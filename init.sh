a77ccfe457ef374ffcd3e63f006bf361cb414956
dankunis <32678968+dankunis@users.noreply.github.com>
2018-04-24 22:21:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
